6d320868bb9bace6bcda55a0ee54a5d5f22255af
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-03-14 10:15:40 +0300

    fix

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category.js");
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, img } = req.body;
+
+    const newCategory = new Category({ name, img });
+    await newCategory.save();
+
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.get("/", async (req, res) => {
-  res.send("Kategoriler getirildi");
+  res.send("Kategoriler getirildi!");
 });
 
 module.exports = router;
