6c6c2414fd44d12d19f3aa31efcf8acbe49c8e64
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-03-14 22:07:33 +0300

    users backend created

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const productRouter = require("./products.js");
 const authRoute = require("./auth");
 const categoryRouter = require("./categories.js");
+const userRoute = require("./users.js");
 
 router.use("/categories", categoryRouter);
 router.use("/auth", authRoute);
 router.use("/products", productRouter);
+router.use("/users", userRoute);
 
 module.exports = router;
