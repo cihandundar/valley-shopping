2c24b2a2449fd6dee855039d33a4789c0e2706fb
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-03-15 20:41:11 +0300

    categories component created

---
+++ frontend/src/components/Catergories/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { message } from "antd";
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/categories`);
+
+        if (response.ok) {
+          const data = await response.json();
+          setCategories(data);
+        } else {
+          message.error("Veri getirme başarısız.");
+        }
+      } catch (error) {
+        console.log("Veri hatası:", error);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  return (
+    <section className="category">
+      <div className="category__container">
+        <div className="category__container__title">
+          <h2>All Categories</h2>
+        </div>
+        <div className="category__item">
+          {categories.map((category) => (
+            <div className="category__wrapper" key={category.id}>
+              <div className="category__img">
+                <img src={category.img} alt="" />
+              </div>
+              <div className="category__name">
+                <h3>{category.name}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Categories;
