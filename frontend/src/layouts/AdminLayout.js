50f566ac375945cfcab94db6c68a4a8d02079ff6
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-03-15 20:41:31 +0300

    router fix

--- frontend/src/layouts/AdminLayout.js
+++ frontend/src/layouts/AdminLayout.js
@@ -100,7 +100,7 @@ const AdminLayout = ({ children }) => {
       icon: <RollbackOutlined />,
       label: "Ana Sayfaya Git",
       onClick: () => {
-        navigate(`/`);
+        window.location.href = "/";
       },
     },
   ];
