5af1e48cb4070e687c6da4f4cc161cd31ab4944b
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-03-16 20:41:48 +0300

    stripe loaded

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,12 @@ const productRouter = require("./products.js");
 const authRoute = require("./auth");
 const categoryRouter = require("./categories.js");
 const userRoute = require("./users.js");
+const paymentRoute = require("./payment.js");
 
 router.use("/categories", categoryRouter);
 router.use("/auth", authRoute);
 router.use("/products", productRouter);
 router.use("/users", userRoute);
+router.use("/payment", paymentRoute);
 
 module.exports = router;
